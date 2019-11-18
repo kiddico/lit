243b4611cc48ded178a83bc5525865bf0238d2ec
Nick <nickchirico07@gmail.com>
2019-11-18 00:55:25 -0500

    Not really a test anymore.

--- video_playback/frames_from_video.py
+++ video_playback/frames_from_video.py
@@ -1,14 +1,13 @@
 import cv2
-test = cv2.VideoCapture('ghost_sample_1.mp4')
+video_in = cv2.VideoCapture('ghost_sample_1.mp4')
 
 frame_num = 0
-while(test.isOpened()):
-    ret, frame = test.read()
+while(video_in.isOpened()):
+    ret, frame = video_in.read()
     if ret == True:
         cv2.imwrite('ghost_sample_1_frames/ghost_sample_1_f{:03d}.png'.format(frame_num), frame)
         frame_num+=1
-    else: 
+    else:
         break
 
-test.release()
-
+video_in.release()
