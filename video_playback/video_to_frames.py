6d043ea034732e0be897b2c53116521a158d7743
Nick <nickchirico07@gmail.com>
2019-11-18 00:30:44 -0500

    padding file names

--- video_playback/video_to_frames.py
+++ video_playback/video_to_frames.py
@@ -1,13 +1,14 @@
 import cv2
-test = cv2.VideoCapture('ghost_sample_2.mp4')
+test = cv2.VideoCapture('ghost_sample_1.mp4')
 
 frame_num = 0
 while(test.isOpened()):
     ret, frame = test.read()
     if ret == True:
-        cv2.imwrite('ghost_sample_2_f{}.png'.format(frame_num), frame)
+        cv2.imwrite('ghost_sample_1_frames/ghost_sample_1_f{:03d}.png'.format(frame_num), frame)
         frame_num+=1
     else: 
         break
 
 test.release()
+
