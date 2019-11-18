8bba30ef64ef6dbdb062a7e89fd4b16ba5ecfc35
Nick <nickchirico07@gmail.com>
2019-11-18 18:54:00 -0500

    TAB COMPLETION.

--- video_playback/README.md
+++ video_playback/README.md
@@ -1,3 +1,3 @@
 Let's take things even further and play a video (slowly) in the terminal.
 
-![right in tha kisser](https://github.com/kiddico/lit/raw/master/video_playback/video_playback.gif "Cool flip though...")
+![right in tha kisser](https://github.com/kiddico/lit/raw/master/video_playback/sample_playback.gif "Cool flip though...")
--- video_playback/video_playback.py
+++ video_playback/video_playback.py
@@ -11,7 +11,7 @@ from os.path import isfile, join
 
 def clamp_and_init(cells):
     # + 35 to brighten things up a bit.
-    clamp = lambda x: int(int((x/255)*16)/16*255) + 35
+    clamp = lambda x: int(int((x/255)*14)/14*255)
     clamped_cells = tuple(tuple(tuple(clamp(val) for val in cell[::-1]) for cell in row) for row in cells)
     clamped_colors = { x for y in clamped_cells for x in y }
 
