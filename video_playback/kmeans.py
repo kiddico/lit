115c10ab597a419e28eb20ecbe2102eb5aa860be
Nick Chirico <Nicholas.Chirico@huntington.com>
2020-02-13 17:24:20 -0500

    Cleanup and comments.

--- video_playback/kmeans.py
+++ video_playback/kmeans.py
@@ -11,10 +11,10 @@ def kmeans(frames):
     color_list = list(color_set)
 
 
-    # Initialize Clusters
     dist = lambda c1,c2: sqrt(sum((px - qx) ** 2.0 for px, qx in zip(c1, c2)))
 
-    centroids = color_list[:255]
+    # Initialize Clusters
+    centroids = color_list[:255]# lists from set have no order. Hopefully this is random enough. ¯\_(ツ)_/¯
     clusters  = defaultdict(list)
     for color in color_list:
         closest = min( ( (dist(color, cent), cent) for cent in centroids ), key=lambda x:x[0])
@@ -24,15 +24,17 @@ def kmeans(frames):
     # Iterate to taste.
     for _ in range(0, 10):
         centroids = []
-        #for colors in clusters.values():
-        for centroid, colors in clusters.items():
-            d = len(colors)
+        for colors in clusters.values():
+            # Find the average of all colors in a cluster and use that as a new centroid.
+            d = len(colors) # Cache instead of len()-ing 3 times.
             centroids.append((
                 int(sum(t[0] for t in colors)/d),
                 int(sum(t[1] for t in colors)/d),
                 int(sum(t[2] for t in colors)/d)
                 ))
 
+        # Wipe out centroids from last iteration.
+        # For some reason calling a default dict's clear() will break it defaultiness
         clusters  = defaultdict(list)
         for color in color_list:
             closest = min( ( (dist(color, cent), cent) for cent in centroids ), key=lambda x:x[0])
