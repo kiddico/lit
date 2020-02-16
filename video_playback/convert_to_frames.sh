e9a53a263c52aa59b74c26f0eb87481a18e3f939
kiddico <nickchirico07@gmail.com>
2020-02-15 19:14:11 -0500

    oh... yeah.

--- video_playback/convert_to_frames.sh
+++ video_playback/convert_to_frames.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env sh
+
+# You'll need ffmpeg install for this to work!
 name=$(echo "$1" | awk -F"." '{print $1}')
 mkdir "$name" >> /dev/null
 ffmpeg -i "$1" -s 640x360 -r 24/1 "$name\\""$name""_f%03d.png"
